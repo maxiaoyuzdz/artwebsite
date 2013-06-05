ee47613f70f5f549020ecbec4fe7ebf8022bd9c6
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2013-06-05 23:36:52 +0800

    save
    添加支付流程，同时对页面不需要的部分做了删减

---
+++ src/main/webapp/assets/js/myscript.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: maxiaoyu
+ * Date: 13-6-4
+ * Time: 下午6:04
+ * To change this template use File | Settings | File Templates.
+ */
+window.onload = function() {
+    document.getElementById("myMessage").innerHTML = "Hello, Cleveland!";
+}
